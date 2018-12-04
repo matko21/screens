58fa75ce640405011fd11135c617b9b603f661d7
Matej Marus <matej.marus@cleverlance.com>
2018-12-04 13:30:09 +0100

    add callback for screen events

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/BaseScreen.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/BaseScreen.kt
@@ -2,13 +2,20 @@ package com.cleverlance.mobile.android.screens.domain
 
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
 
 abstract class BaseScreen : Screen() {
 
-    lateinit var dispose: CompositeDisposable
+    lateinit var dispose: Disposable
+    protected open val onAfterShow: (() -> Unit)? = null
+    protected open val onAfterDisposed: (() -> Unit)? = null
 
     fun onShow(dispose: Disposable) {
-        this.dispose = CompositeDisposable(dispose)
+        this.dispose = CompositeDisposable(
+            dispose,
+            Disposables.fromAction { onAfterDisposed?.invoke() }
+        )
+        onAfterShow?.invoke()
     }
 
     /** @return true if back action was consumed by the call */
