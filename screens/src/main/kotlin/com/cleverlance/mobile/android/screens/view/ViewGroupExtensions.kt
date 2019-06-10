f90b1716e3f04ce686fab57b59f4f724ccafb04f
Matej Marus <matej.marus@cleverlance.com>
2019-06-10 09:47:27 +0200

    use transaction manager for changing screens

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/view/ViewGroupExtensions.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/view/ViewGroupExtensions.kt
@@ -2,12 +2,16 @@ package com.cleverlance.mobile.android.screens.view
 
 import android.app.Activity
 import android.view.ViewGroup
+import androidx.transition.Scene
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
 import com.cleverlance.mobile.android.screens.domain.Screen
 import com.cleverlance.mobile.android.screens.domain.createSelfBindingView
 
-fun ViewGroup.setScreen(screen: Screen) {
-    removeAllViews()
-    addScreen(screen)
+fun ViewGroup.setScreen(screen: Screen, transaction: Transition? = null) {
+    screen.createSelfBindingView(this, context as Activity)?.let {
+        TransitionManager.go(Scene(this, it), transaction)
+    }
 }
 
 fun ViewGroup.addScreen(screen: Screen) {
