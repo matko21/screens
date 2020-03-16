30b269741cd25cc23e9d57e46c581b40945cce82
Matej Marus <matej.marus@cleverlance.com>
2020-03-16 17:43:00 +0100

    use fragment activity instead of android activity

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/view/ScreenPagerAdapter.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/view/ScreenPagerAdapter.kt
@@ -1,8 +1,8 @@
 package com.cleverlance.mobile.android.screens.view
 
-import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager.widget.PagerAdapter
 import com.cleverlance.mobile.android.screens.domain.Screen
 import com.cleverlance.mobile.android.screens.domain.createSelfBindingView
@@ -22,5 +22,5 @@ abstract class ScreenPagerAdapter : PagerAdapter() {
     override fun isViewFromObject(view: View, any: Any) = view == any
 
     private fun createScreenContainer(container: ViewGroup, screen: Screen) =
-            screen.createSelfBindingView(container, container.context as Activity)
+            screen.createSelfBindingView(container, container.context as FragmentActivity)
 }
